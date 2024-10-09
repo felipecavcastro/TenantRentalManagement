ede3a4a32e6b208f2b603345eb03547adaaa5dfa
Felipecavcastro <felipecavcastro@gmail.com>
2024-10-09 18:00:14 -0300

    Crição do ContractRepository

---
+++ src/main/java/com/felipe/castro/TenantRentalManagement/repositories/ContractRepository.java
@@ -0,0 +1,11 @@
+package com.felipe.castro.TenantRentalManagement.repositories;
+
+import com.felipe.castro.TenantRentalManagement.models.ContractModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ContractRepository extends JpaRepository<ContractModel, UUID> {
+}
